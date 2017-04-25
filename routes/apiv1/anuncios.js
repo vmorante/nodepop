09aa4b96bbe36abffd48dbd7bccb10a7c1cc8d78
vmorante <veronica.morante.pindado@gmail.com>
2017-04-25 11:32:40 +0200

    get y post de usuarios

--- routes/apiv1/anuncios.js
+++ routes/apiv1/anuncios.js
@@ -9,6 +9,11 @@ const mongoose = require('mongoose');
 //le pedimos a mongoose el modelo de anuncio
 const Anuncio = mongoose.model('Anuncio');
 
+//jSON web token
+const jwtAuth = require('../../lib/jwtAuth');
+router.use(jwtAuth);
+
+
 //var anuncio = new Anuncio({ nombre: 'Bicicleta', venta: true, precio: '230.15', foto: 'bici.jpg', tags: ['lifestyle', 'mobile'] })
 
 /*anuncio.save(function(err, anuncioCreado) {
@@ -17,15 +22,17 @@ const Anuncio = mongoose.model('Anuncio');
 });
 */
 router.get('/', (req, res, next) => {
-    console.log(req.usuario_id);
+
     const tags = req.query.tags;
     const venta = req.query.venta;
     const precio = req.query.precio;
+    const nombre = req.query.nombre;
     const limit = parseInt(req.query.limit);
     const skip = parseInt(req.query.skip);
     const select = req.query.select;
     const sort = req.query.sort;
-    var precioSplit = precio.split("-");
+    //const start = req.query.start;
+    //var precioSplit = precio.split("-");
     const menor = /-\d/;
     const mayor = /\d-/;
     const entre = /\d-\d/;
@@ -38,6 +45,7 @@ router.get('/', (req, res, next) => {
         criterios.venta = venta;
     }
     if (precio) {
+        var precioSplit = precio.split("-");
         if (entre.test(precio)) {
             criterios.precio = { '$gte': precioSplit[0], '$lte': precioSplit[1] };
         } else if (mayor.test(precio)) {
@@ -47,6 +55,9 @@ router.get('/', (req, res, next) => {
         } else if (igual.test(precio)) {
             criterios.precio = precioSplit[0];
         }
+    }
+    if (nombre) {
+        criterios.nombre = new RegExp('^' + req.query.nombre, "i");
     }
 
 
--- routes/apiv1/usuarios.js
+++ routes/apiv1/usuarios.js
@@ -5,9 +5,95 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
+const crypto = require('../../lib/encriptar');
 
 //le pedimos a mongoose el modelo de agente
 const Usuario = mongoose.model('Usuario');
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
 
+
+
+router.post('/authenticate', (req, res, next) => {
+    //recibimos credenciales
+    const correo = req.body.email;
+    const clave = req.body.clave;
+
+    //buscamos al usuario en la bd
+    Usuario.findOne({ correo: correo }).exec((err, usuario) => {
+        if (err) {
+            next(err);
+
+            return;
+        }
+        if (!usuario) {
+            res.json({ success: false, error: 'Credenciales incorrectas' });
+            return;
+        }
+        //comprobamos su clave
+        var passEncriptada = crypto(correo, clave)
+        if (passEncriptada !== usuario.clave) {
+            res.json({ success: false, error: 'Credenciales incorrectas' });
+            return;
+        }
+
+        //creamos un token JWT  
+        jwt.sign({ usuario_id: usuario._id }, config.jwtSecret, config.jwtConfig,
+            (err, token) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+                //se lo devolvemos
+                res.json({ succes: true, token: token })
+            });
+    });
+});
+
+router.post('/registro', (req, res, next) => {
+    //recibimos credenciales
+    const correo = req.body.email;
+    const clave = req.body.clave;
+    const nombre = req.body.nombre
+
+    //buscamos al usuario en la bd
+    Usuario.findOne({ correo: correo }).exec((err, usuario) => {
+        if (err) {
+            next(err);
+
+            return;
+        }
+        if (usuario) {
+            res.json({ success: false, error: 'Usuario existente' });
+            return;
+        }
+        //comprobamos su clave
+        var passEncriptada = crypto(correo, clave)
+        if (!clave || !nombre || !correo) {
+            res.json({ success: false, error: 'Rellene todos los campos' });
+            return;
+        }
+
+        var usuario = new Usuario({ "correo": correo, "clave": clave, "nombre": nombre });
+        var passEncriptada = crypto(correo, clave);
+        usuario.clave = passEncriptada;
+        usuario.save(function(err, usuarioCreado) {
+            if (err) throw err;
+            console.log('Usuario ' + usuarioCreado.nombre + ' creado');
+        });
+
+
+        //creamos un token JWT  
+        jwt.sign({ usuario_id: usuario._id }, config.jwtSecret, config.jwtConfig,
+            (err, token) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+                //se lo devolvemos
+                res.json({ succes: true, token: token })
+            });
+    });
+});
 
 module.exports = router;
